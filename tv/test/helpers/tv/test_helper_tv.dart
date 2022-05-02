8beee6c07b35f3a3b573d29564fb85dff5a0fc4c
Daniza Salsabila <87085616+danizasalsabila@users.noreply.github.com>
2022-05-02 18:46:57 +0700

    Update test_helper_tv.dart

--- tv/test/helpers/tv/test_helper_tv.dart
+++ tv/test/helpers/tv/test_helper_tv.dart
@@ -1,3 +1,4 @@
+import 'package:core/ssl/ssl_pining.dart';
 import 'package:tv/data/datasources/datasource_tv/db/database_helper_tvseries.dart';
 import 'package:tv/data/datasources/datasource_tv/tv/tv_local_datasource.dart';
 import 'package:tv/data/datasources/datasource_tv/tv/tv_remote_datasource.dart';
@@ -11,6 +12,7 @@ import 'package:http/http.dart' as http;
   TvLocalDataSource,
   DatabaseHelperTv,
 ], customMocks: [
-  MockSpec<http.Client>(as: #MockHttpClient)
+  MockSpec<http.Client>(as: #MockHttpClient),
+  MockSpec<ApiIOClient>(as: #MockApiIOClient)
 ])
 void main() {}
