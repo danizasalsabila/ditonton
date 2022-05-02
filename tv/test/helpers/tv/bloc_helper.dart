4b8db7223653cac5ad4fb020e44a0c0ebfe49531
Daniza Salsabila <87085616+danizasalsabila@users.noreply.github.com>
2022-05-02 18:45:52 +0700

    Update bloc_helper.dart

--- tv/test/helpers/tv/bloc_helper.dart
+++ tv/test/helpers/tv/bloc_helper.dart
@@ -1,10 +1,16 @@
 import 'package:mockito/annotations.dart';
-import 'package:http/http.dart' as http;
 import 'package:tv/domain/usecases/tv/get_now_playing_tv.dart';
 import 'package:tv/domain/usecases/tv/get_popular_tv.dart';
 import 'package:tv/domain/usecases/tv/get_top_rated_tv.dart';
+import 'package:tv/domain/usecases/tv/get_tv_detail.dart';
 import 'package:tv/domain/usecases/tv/get_tv_recommendations.dart';
+import 'package:tv/domain/usecases/tv/get_watchlist_status_tv.dart';
 import 'package:tv/domain/usecases/tv/get_watchlist_tv.dart';
+import 'package:tv/domain/usecases/tv/remove_watchlist.dart';
+import 'package:tv/domain/usecases/tv/save_watchlist.dart';
+import 'package:tv/presentation/bloc/popular_tv/popular_tv_bloc.dart';
+import 'package:tv/presentation/bloc/top_rated/bloc/top_rated_tv_bloc.dart';
+import 'package:tv/presentation/bloc/watchlist/bloc/watchlist_tv_bloc.dart';
 
 @GenerateMocks([
   GetNowPlayingTv,
@@ -12,7 +18,13 @@ import 'package:tv/domain/usecases/tv/get_watchlist_tv.dart';
   GetTvRecommendations,
   GetTopRatedTv,
   GetWatchlistTv,
-], customMocks: [
-  MockSpec<http.Client>(as: #MockHttpClient)
+  GetTvDetail,
+  GetWatchListStatusTv,
+  SaveWatchlistTv,
+  RemoveWatchlistTv,
+  PopularTvBloc,
+  TopRatedTvBloc,
+  WatchlistTvBloc
+
 ])
 void main() {}
