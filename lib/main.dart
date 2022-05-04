760fa5e04b40e1de24867dc43735733803f1f33e
Daniza Salsabila <87085616+danizasalsabila@users.noreply.github.com>
2022-05-04 18:00:26 +0700

    Update main.dart
    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -33,10 +33,13 @@ import 'package:movie/presentation/pages/watchlist_movies_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:ditonton/injection.dart' as di;
 //DANIZA SALSABILA
 
-void main() {
+void main() async  {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   di.init();
   runApp(MyApp());
 }
