22038c0905ab055b867065a5e1a27f1edfab5398
Daniza Salsabila <87085616+danizasalsabila@users.noreply.github.com>
2022-05-06 12:18:04 +0700

    Update json_reader.dart

--- movie/test/json_reader.dart
+++ movie/test/json_reader.dart
@@ -5,7 +5,7 @@ String readJson(String name) {
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  if (dir.endsWith('core')){
+  if (dir.endsWith('movie')){
     return File('$dir/test/$name').readAsStringSync();
   }
   return File('$dir/movie/test/$name').readAsStringSync();
